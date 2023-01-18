c760a886cb1a4b8d7584ecc511fd67a448bd9789
Marla Spencer <marla.spencer@nhm.ac.uk>
2023-01-18 12:05:03 +0000

    More text added

--- Script1.R
+++ Script1.R
@@ -1,3 +1,5 @@
 #script trial
 
-#adding text to the test script file# save the script to prompt
+#adding text to the test script file
+# save the script to prompt
+#Finally learning git and github! Yay
