2709eff10532ce790a5c264a3be25c0ce58cbe66
Marla Spencer <marla.spencer@nhm.ac.uk>
2023-01-18 12:07:19 +0000

    Practicing the cycle modify --> save --> commit

--- Script1.R
+++ Script1.R
@@ -1,5 +1,6 @@
 #script trial
-
+#--> Add --> commit -->
 #adding text to the test script file
 # save the script to prompt
 #Finally learning git and github! Yay
+#practicing the cycle  modify --> save --> commit
